556234f60ad4f690d847e4987fb90b5af5a49c55
higorxi <higorgiovane7@gmail.com>
2024-06-14 01:25:39 -0300

    new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UserModule } from './modules/user.modules';
 import { AuthModule } from './modules/auth.modules';
+import { BadgeModule } from './modules/badge.modules';
+import { GameScoreModule } from './modules/gameScore.modules';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     UserModule, 
     AuthModule,
+    BadgeModule,
+    GameScoreModule
   ],
   controllers: [],
   providers: [],
