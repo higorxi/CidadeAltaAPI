4a803a96ec97985494721bb0d7dd134218a6593d
higorxi <higorgiovane7@gmail.com>
2024-06-17 17:56:24 -0300

    test deploy

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -7,6 +7,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database:  process.env.DB_NAME,
+  ssl: true,
+  logging: true,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
